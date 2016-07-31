0cdf8d647ca587fef5523373dbf0c508dba79c46
SeanChen <livebreeze@gmail.com>
2016-07-31 11:01:46 +0800

    add header components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header'; // 預設載入 index.ts
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HeaderComponent]
 })
 export class AppComponent {
-  title = '保哥的title';
+
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-header',
+  templateUrl: 'header.component.html',
+  styleUrls: ['header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  title = '保哥的title';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
