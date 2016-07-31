ff3455dff67690c14bfbfd927b89d6b00b49eb9b
SeanChen <livebreeze@gmail.com>
2016-07-31 14:30:21 +0800

    10 練習使用 *ngFor 將資料輸出到頁面上 (同時建立 ArticleComponent 元件)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header'; // 預設載入 index.ts
+import { ArticleComponent } from './article';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [HeaderComponent]
+  directives: [HeaderComponent, ArticleComponent]
 })
 export class AppComponent {
 
